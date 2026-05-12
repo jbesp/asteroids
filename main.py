68d440df2419e313da188b4c2f0ddaf5cee49a7c
jbesp <javibaeza@alumnos.uai.cl>
2026-05-12 17:22:28 -0400

    add player and group

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from logger import log_state
 from constants import *
+from player import Player
+
 
 
 
@@ -12,19 +14,31 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Clock = pygame.time.Clock()
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group() 
+    Player.containers = (drawable, updatable)
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    
 
     while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
+        updatable.update(dt)
         screen.fill("black")
+
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.flip()
         Clock.tick(60)
         dt = Clock.tick(60) / 1000.0
-        print(dt)
+        
         
 if __name__ == "__main__":
     main()
